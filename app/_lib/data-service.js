68a354f1b664ded9f11b97fa9aeb2b2938d7adaf
Janika Savela <janika_s@hotmail.com>
2024-12-22 11:26:46 +0200

    varauksen tekeminen onnistuu

--- app/_lib/data-service.js
+++ app/_lib/data-service.js
@@ -161,7 +161,7 @@ export async function createGuest(newGuest) {
   return data
 }
 
-export async function createBooking(newBooking) {
+/* export async function createBooking(newBooking) {
   const { data, error } = await supabase
     .from('bookings')
     .insert([newBooking])
@@ -175,7 +175,7 @@ export async function createBooking(newBooking) {
   }
 
   return data
-}
+} */
 
 /////////////
 // UPDATE
